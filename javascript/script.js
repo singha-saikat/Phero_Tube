02512aadc08455a71d26b3e1547d9bb2f8e7c4ec
Saikat <sachinsingha7899@gmail.com>
2023-09-01 12:32:16 +0600

    try to do something but not progress

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,24 @@
 console.log('welcome');
+const handleCategory = async () => {
+   const response = await fetch ('https://openapi.programming-hero.com/api/videos/categories');
+   const data = await response.json();
+   console.log(data.data);
+   const categoriesContainer = document.getElementById('category-container');
+   const div = document.createElement('div');
+   data.data?.forEach((catagori) => {
+      console.log(catagori);
+      div.innerHTML =`
+   <button class="btn">${catagori.category}</button>
+   
+   
+   `
+   
+   })
+   categoriesContainer.appendChild(div);
+ } 
 
 
 
+handleCategory();
 
+ 
